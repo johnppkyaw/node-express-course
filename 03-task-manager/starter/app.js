c5ecef0d623f2d9ed39ab2eb28586174b8c03889
John Kyaw <jjpyae@gmail.com>
2025-08-10 22:42:55 -0400

    change app.js

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const app = express();
 const tasks = require('./routes/tasks.js');
-<<<<<<< HEAD
 const connectDB = require('./db/connect');
 require('dotenv').config()
-=======
->>>>>>> 0082cefe3714a2448a848012ebefdc9e8acf06d0
 
 //middleware
 app.use(express.json());
@@ -15,7 +12,6 @@ app.get('/hello', (req, res) => {
   res.send('Task Manager App')
 });
 
-<<<<<<< HEAD
 app.use('/api/v1/tasks', tasks);
 
 const port = 3000;
@@ -30,14 +26,3 @@ const start = async () => {
 }
 
 start()
-=======
-
-app.use('/api/v1/tasks', tasks);
-
-
-
-
-const port = 3000;
-
-app.listen(port, console.log(`Server is listening on port ${port}...`))
->>>>>>> 0082cefe3714a2448a848012ebefdc9e8acf06d0
