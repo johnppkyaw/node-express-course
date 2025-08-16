2e31184adf41887bfdadd0b625de2baf79678d98
John Kyaw <jjpyae@gmail.com>
2025-08-16 14:41:32 -0400

    fix merge conflicts

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
 const Task = require('../models/Task');
 
-=======
->>>>>>> 0082cefe3714a2448a848012ebefdc9e8acf06d0
 const getAllTasks = (req, res) => {
   res.send('get all task')
 }
 
-<<<<<<< HEAD
 const createTask = async (req, res) => {
   try {
     const task = await Task.create(req.body);
@@ -18,15 +14,7 @@ const createTask = async (req, res) => {
 }
 
 const getTask = (req, res) => {
-  res.json({ id: req.params.id })
-=======
-const createTask = (req, res) => {
-  res.send('create task')
-}
-
-const getTask = (req, res) => {
   res.send('get a single task')
->>>>>>> 0082cefe3714a2448a848012ebefdc9e8acf06d0
 }
 
 const updateTask = (req, res) => {
