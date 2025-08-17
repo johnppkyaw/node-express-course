36295b716d6ee643c4775089acb4f21d4182ac5a
John Kyaw <jjpyae@gmail.com>
2025-08-17 16:47:29 -0400

    complete week 6

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,29 +1,46 @@
 const Task = require('../models/Task');
+const asyncWrapper = require('../middleware/async');
+const { CustomAPIError, createCustomError } = require('../errors/custom-error');
 
-const getAllTasks = (req, res) => {
-  res.send('get all task')
-}
+const getAllTasks = asyncWrapper(async (req, res) => {
+  const tasks = await Task.find({});
+  res.status(200).json({ tasks });
+})
 
-const createTask = async (req, res) => {
-  try {
-    const task = await Task.create(req.body);
-    res.status(201).json(task)
-  } catch (error) {
-    res.status(500).json({msg: error});
+const createTask = asyncWrapper(async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json(task)
+})
+
+const getTask = asyncWrapper(async (req, res, next) => {
+  const {id:taskId} = req.params;
+  const task = await Task.findOne({ _id: taskId })
+  if(!task) {
+    return next(createCustomError(`No task with id: ${taskId}`, 404))
   }
-}
+  res.status(200).json({task});
+})
 
-const getTask = (req, res) => {
-  res.send('get a single task')
-}
+const updateTask = asyncWrapper(async (req, res) => {
+  const {id:taskId} = req.params;
+  const task = await Task.findOneAndUpdate({_id: taskId, }, req.body, {
+    new:true, 
+    runValidators: true
+  })
+  if(!task) {
+    return next(createCustomError(`No task with id: ${taskId}`, 404))
+  }
+  res.status(200).json({task})
+})
 
-const updateTask = (req, res) => {
-  res.send('update task')
-}
-
-const deleteTask = (req, res) => {
-  res.send('delete task')
-}
+const deleteTask = asyncWrapper(async (req, res) => {
+  const {id:taskId} = req.params;
+  const task = await Task.findOneAndDelete({ _id: taskId })
+  if(!task) {
+    return next(createCustomError(`No task with id: ${taskId}`, 404))
+  }
+  res.status(200).json({task});
+})
 
 module.exports = {
   getAllTasks,
